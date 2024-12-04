4055f6747f73ee7b95c2c3386c2793013c291535
Jatin <jatinrajvani.cg@gmail.com>
2024-12-04 15:25:08 +0530

    4 added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Ludo from './ludo.jsx'
+import Ludo from './Ludo.jsx'
 
 
 createRoot(document.getElementById('root')).render(
