b8887503b068934e4ec51a8505f2487577f165e9
Jatin <jatinrajvani.cg@gmail.com>
2024-12-04 14:57:38 +0530

    new added

---
+++ src/Ludo.jsx
@@ -0,0 +1,174 @@
+import react from 'react';
+import './ludo.css'
+
+function Ludo(){
+    return(
+        <>
+ <div className='main'>
+    
+ <div className="parent1">
+          <div className="child1">
+            <div className="subchild">
+                <div className="subsubchild"></div>
+                <div className="subsubchild"></div>
+                <div className="subsubchild"></div>
+                <div className="subsubchild"></div>
+            </div>
+          </div>
+          <div className="childbox" id="boxes" >
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            
+          </div>
+          <div className="child1" id="green">
+            <div className="subchild">
+                <div className="subsubchild" id="greenchild" ></div>
+                <div className="subsubchild" id="greenchild" ></div>
+                <div className="subsubchild" id="greenchild" ></div>
+                <div className="subsubchild" id="greenchild" ></div>
+                
+            </div>
+          </div>
+        </div>
+        <div className="parent2">
+            <div className="child2" id="midbox1">
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                
+            </div>
+            <div className="child2" id="tri">
+
+              
+            </div>
+            <div className="child2">
+              <div className="midboxchild1" id="midbox2">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+                <div className="midboxchild1">1</div>
+            </div>
+        </div>
+
+        
+        <div className="parent3">
+          <div className="child1" id="blue">
+            <div className="subchild"  >
+                <div className="subsubchild" id="bluechild"></div>
+                <div className="subsubchild" id="bluechild"></div>
+                <div className="subsubchild" id="bluechild"></div>
+                <div className="subsubchild" id="bluechild"></div>
+                
+            </div>
+          </div>
+          <div className="childbox" id="boxes" >
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            <div className="boxchild">1</div>
+            
+          </div>
+
+          <div className="child1" id="yellow">
+            <div className="subchild">
+                <div className="subsubchild" id="yellowchild"></div>
+                <div className="subsubchild" id="yellowchild"></div>
+                <div className="subsubchild" id="yellowchild"></div>
+                <div className="subsubchild" id="yellowchild">  </div>
+             
+            </div>
+          </div>
+
+         
+          
+
+
+        </div>
+        <div><img src={"../public/Safari-3.png"} alt="" /></div>
+ </div>
+        </>
+    )
+}
+export default Ludo
